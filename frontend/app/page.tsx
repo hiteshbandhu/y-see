c995bb0cfc9c2a46089813f27f50fc70c3a9f3cc
hiteshbandhu <bandhuhitesh52003@gmail.com>
2024-07-07 17:11:05 +0530

    initilised frontend initialised backend

---
+++ frontend/app/page.tsx
@@ -0,0 +1,18 @@
+import Image from "next/image";
+
+export default function Home() {
+  return(
+    <div className="bg-black-500 m-[3rem] text-white">
+      <h1 className="text-3xl">Y-SEE</h1>
+    <p className="text-xl text-red-600">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus lacinia odio vitae vestibulum vestibulum. Cras venenatis euismod malesuada. Curabitur sit amet fermentum orci. Nulla facilisi. Integer vel augue lacus. Phasellus blandit turpis ac lectus cursus, sed bibendum libero facilisis. Integer interdum justo nec tellus placerat, sed fringilla lorem suscipit. Fusce ac dui sit amet dolor hendrerit dapibus. Duis vehicula dapibus odio, ac feugiat tortor imperdiet nec. Pellentesque et metus nec libero dignissim tristique.
+    Sed id odio sit amet risus fringilla commodo. Proin non nisl nec dui dictum luctus. Vivamus ultricies, tortor non sagittis facilisis, lectus nunc volutpat urna, ac consequat erat tortor sit amet elit. Maecenas vehicula ligula sit amet ex vestibulum, a interdum sem bibendum. Etiam accumsan purus sed urna tincidunt, nec pretium lectus consequat. Ut id leo purus. Integer auctor sapien non diam varius, eget efficitur ligula suscipit. Mauris vel nulla non justo consectetur posuere. Phasellus vitae orci at lacus finibus mollis. Mauris fringilla dolor in erat posuere, a laoreet nulla vehicula. Phasellus sed varius libero. Donec sit amet ligula bibendum, gravida lacus eget, pretium mauris. Curabitur vel dignissim justo, nec dictum augue.
+      </p>
+    </div>
+    
+
+
+
+
+
+  )
+}
